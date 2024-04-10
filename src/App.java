1bcfe17ad2ed9b6ff56a2456b17ad9a2a5136d17
Samuel <samueligcorp@gmail.com>
2024-04-10 18:36:15 +0200

    avance

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /*El programa debe permitirnos ingresar un vehiculo para su reparación, marcar que pieza o piezas estan averiadas y mostrar la informacion de un vehiculo */
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        while (true) {
+            System.out.print("Ingrese la opción: ");
+            opcion = scanner.nextInt();
+            switch (opcion) {
+                case 1:
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+    
     }
 }
+
+//System.out.println("Saliendo...");
+//System.exit(0);
